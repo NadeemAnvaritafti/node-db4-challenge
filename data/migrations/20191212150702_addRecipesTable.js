d3159d9149f1c5db868672c762ebb5d1363785ad
Nadeem Anvaritafti <nanvari@live.com>
2019-12-12 15:24:30 -0600

    added migration table data

---
+++ data/migrations/20191212150702_addRecipesTable.js
@@ -0,0 +1,53 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('dishes', tbl => {
+        tbl.increments();
+        tbl.string('name', 255)
+            .notNullable()
+            .unique();
+    })
+    .createTable('ingredients', tbl => {
+        tbl.increments();
+        tbl.string('name', 255)
+            .notNullable()
+            .unique();
+    })
+    .createTable('steps', tbl => {
+        tbl.increments();
+        tbl.text('instructions')
+            .notNullable();
+
+        // Foreign Key
+        tbl.integer('dish_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('dishes')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+    })
+    .createTable('recipes', tbl => {
+        tbl.primary(['dish_id', 'ingredient_id']);
+        tbl.integer('dish_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('dishes');
+        tbl.integer('ingredient_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('ingredients');
+        tbl.float('ingredient_quantity')
+            .notNullable();
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists("dishes")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("steps")
+    .dropTableIfExists("recipes") 
+};
